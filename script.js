ac71b420f00f4048a793e6056b11cb6d4d8c1e53
sufrusdraker <99483010+sufrusdraker@users.noreply.github.com>
2025-03-02 20:12:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function showContent(contentId) {
 async function sendMessage() {
     const userInput = document.getElementById("user-input").value;
     const chatHistory = document.getElementById("chat-history");
-    const apiUrl = "https://ccc6-2001-8a0-f4da-f200-8182-7f1a-fbc4-5471.ngrok-free.app";
+    const apiUrl = "https://9e0f-2001-8a0-f4da-f200-8182-7f1a-fbc4-5471.ngrok-free.app";
 
     if (userInput.trim() !== "") {
         // Adiciona a mensagem do usuário ao histórico
